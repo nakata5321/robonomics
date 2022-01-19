6abbf50a094cebd5c0c163b4a7dc59f6252bab0e
akru <akru@users.noreply.github.com>
2022-01-19 12:18:41 +0000

    deploy: 1bcb89e7b8b980159146381df31718ba3daff364

---
+++ implementors/pallet_vesting/pallet/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["main_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"main_runtime/struct.Runtime.html\" title=\"struct main_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["main_runtime::Runtime"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
