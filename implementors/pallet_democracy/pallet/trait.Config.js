f4793b87244233f064a3d6e5b1cddcceed7b628d
akru <akru@users.noreply.github.com>
2022-01-20 08:40:16 +0000

    deploy: 7937f3d5d110f3afbfaf5402e84f722e923eba51

--- implementors/pallet_democracy/pallet/trait.Config.js
+++ implementors/pallet_democracy/pallet/trait.Config.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["local_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"local_runtime/struct.Runtime.html\" title=\"struct local_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["local_runtime::Runtime"]}];
 implementors["main_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"main_runtime/struct.Runtime.html\" title=\"struct main_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["main_runtime::Runtime"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
